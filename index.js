32e62ae422e04d1bb292039a831c0a1bd80c3e74
shawon012 <shawon.shakib01@gmail.com>
2023-05-02 19:09:47 +0600

    cors installed

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const port =process.env.PORT || 5000;
 const chefs = require('./data/chefs.json');
 const recipes = require('./data/recipe.json');
 
-app.use(cors())
+app.use(cors());
 
 app.get('/', (req, res)=>{
-    res.send('The server is running')
+    res.send('The server is running');
 });
 app.get('/chefs', (req, res)=>{
     res.send(chefs);
